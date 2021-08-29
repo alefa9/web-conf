2d91ff0a261683d27fa264757e63347cf4badef9
Alejo Farias <alejofarias99@gmail.com>
2021-08-28 22:44:40 -0300

    add map

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@
 
     document.addEventListener('DOMContentLoaded', function () {
 
+        var map = L.map('mapa').setView([-34.603753, -58.381616], 20);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([-34.603753, -58.381616]).addTo(map)
+            .bindPopup('BsAsWebCamp 2021 <br/> Boletos ya disponibles')
+            .openPopup();
+
         // Campos datos usuario
 
         var nombre = document.getElementById('nombre');
